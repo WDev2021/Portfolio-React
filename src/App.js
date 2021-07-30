54fda8a72747dd67aaa13bb12a67f3fe1b96a457
Jaime <salamanca10@me.com>
2021-07-29 20:14:25 -0400

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          James' react-app template
+          James' react-app template final
         </p>
         <a
           className="App-link"
